36e2e3f351bdc022160688fecdb4f67f6b1e346c
sojurner <pykim0591@gmail.com>
2018-07-11 22:09:46 -0600

    Attempted Trie find and insert methods, created node class

---
+++ test/Trie-test.js
@@ -0,0 +1,23 @@
+const { assert } = require('chai');
+const Trie = require('../lib/Trie.js');
+
+describe ('Trie', function () {
+
+	it('add nodes', function() {
+		const trie = new Trie()
+		
+		assert
+	});
+
+	it('should add nodes', function() {
+
+	});
+
+	it('should add nodes', function() {
+
+	});
+
+	it('should add nodes', function() {
+
+	})
+	})
